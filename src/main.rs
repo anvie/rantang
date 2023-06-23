4c293a1c79a7784826848f0d65f73b16cbc5c342
anvie <robinsyihab@gmail.com>
2023-06-23 21:43:08 +0700

    bump version 0.0.4

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ async fn save_file(req: HttpRequest, mut payload: Multipart) -> Result<HttpRespo
     let nonce_from_client = get_header_value("X-Nonce", &req)?;
     let nonce = format!("{}", nonce::nonce());
 
-    debug!("NONCE: client: {}, server: {}", nonce_from_client, nonce);
+    debug!("NONCE: client <> server - {} <> {}", nonce_from_client, nonce);
 
     if !crypto::verify_signature(secret_key.as_bytes(), nonce.as_bytes(), signature) {
         return Err(error::ErrorBadRequest(
